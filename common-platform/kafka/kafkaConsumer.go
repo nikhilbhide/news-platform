585630ef76c9fe5b521fe3d0ee7f56869d3d0991
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-10 22:08:31 +0530

    added support for consuming and processing articles

--- common-platform/kafka/kafkaConsumer.go
+++ common-platform/kafka/kafkaConsumer.go
@@ -20,7 +20,7 @@ func Close(consumer *kafka.Consumer, msg *kafka.Message) {
 
 //subscribe to the topic
 //it returns the message and consumer
-func createConsumer(topic string) *kafka.Consumer {
+func CreateConsumer(topic string) *kafka.Consumer {
 	//create at least consumer
 	//enable.auto.commit is true and sync processing and auto.commit.interval.ms = 10000 -> atleast once/atmost once
 	//enable.auto.commit is true and async processing and auto.commit.interval.ms = 0 -> atmost once
@@ -29,7 +29,7 @@ func createConsumer(topic string) *kafka.Consumer {
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers":  "localhost",
 		"group.id":           "article_scraper_processor",
-		"auto.offset.reset":  "latest",
+		"auto.offset.reset":  "earliest",
 		"enable.auto.commit": "false",
 	})
 

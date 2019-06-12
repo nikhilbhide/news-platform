c2f36c4ff7c723c979cf9540e9ffb7b410782257
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-12 22:53:38 +0530

    Added scraper for https://newsapi.org/sources

--- news-web-scraper/googleNewsApiCountryScraper.go
+++ news-web-scraper/googleNewsApiCountryScraper.go
@@ -3,41 +3,66 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
-// This will get called for each HTML element found
-func processElement(index int, element *goquery.Selection) {
-	// See if the href attribute exists on the element
-	href, exists := element.Attr("href")
-	if exists {
-		fmt.Println(href)
-	}
+var countrySet map[string]bool
+var countires []string
+
+func GetCountries() []string {
+	return countires
 }
 
 func main() {
-	// Make HTTP GET request
+	countrySet = make(map[string]bool) // New empty set
+
+	// Make HTTP request
 	response, err := http.Get("https://newsapi.org/sources")
+	//response, err := client.Do(request)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer response.Body.Close()
 
-	/*// Create a goquery document from the HTTP response
+	// Create a goquery document from the HTTP response
 	document, err := goquery.NewDocumentFromReader(response.Body)
 	if err != nil {
 		log.Fatal("Error loading HTTP response body. ", err)
 	}
+	// Make request
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
 
+	// Copy data from the response to standard output
+	_, err = io.Copy(os.Stdout, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Println(response)
 	// Find all links and process them with the function
 	// defined earlier
-	document.Find("countries-and-categories").Each(processElement)*/
+	getCountries := func(index int, element *goquery.Selection) {
+		// See if the href attribute exists on the element
+		country, exists := element.Attr("data-country")
+		if exists {
+			countrySet[country] = true
+		}
+	}
 
-	// Read response data in to memory
-	body, err := ioutil.ReadAll(response.Body)
-	//document.Find("countries-and-categories").Each(processElement)
-	fmt.Println(string(body))
+	document.Find("div").Each(getCountries)
+	fmt.Println(countrySet)
 
+	for key, _ := range countrySet {
+		countires = append(countires, key)
+	}
+
+	for _, value := range GetCountries() {
+		fmt.Println(value)
+	}
 }

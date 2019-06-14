830cebc7fa8652c09c66cc37454eb03a39439c11
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-14 21:55:47 +0530

    added logic to insert articles data into the cassandra table and a sleep for go routine

--- news-store/services/HeadlinesConsumer.go
+++ news-store/services/HeadlinesConsumer.go
@@ -34,23 +34,29 @@ func ListenAndProcessArticles() {
 	for {
 		msg, _ := consumer.ReadMessage(-1)
 
+		//process messages
 		if msg != nil {
 			//transform the message into articles
 			res := Newsheadlines{}
 			if err := json.Unmarshal(msg.Value, &res); err != nil {
 				panic(err)
 			}
+
+			//connect to cassandra cluster and insert the articles into news_headlines
 			session := cassandra.ConnectToCluster()
 			defer cassandra.CloseSession(session)
 			for _, article := range res.Articles {
 				if err := session.Query(`
-      					INSERT INTO users (country, author, content, date, description, url, published_at) VALUES (?, ?, ?,?, ?, ?, ?)`,
-					"us", article.Author, article.Content, article.PublishedAt, article.Description, article.URL, article.PublishedAt).Exec(); err != nil {
+      					INSERT INTO news_headlines (country, author, content, date, description, url, published_at) VALUES (?, ?, ?,?, ?, ?, ?)`,
+					"us", article.Author, article.Content, article.PublishedAt.Format("01-02-2006"), article.Description, article.URL, article.PublishedAt).Exec(); err != nil {
 					fmt.Println(err)
 				} else {
 
 				}
 			}
 		}
+
+		//sleep for 1000
+		time.Sleep(time.Microsecond)
 	}
 }

b8538eed30c50951d26b0120fb7f9c4eff7aa4f5
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-17 22:28:30 +0530

    added configuration support

--- news-collector/config/envConfig.go
+++ news-collector/config/envConfig.go
@@ -1,42 +1,15 @@
 package config
 
 import (
+	"github.com/nik/news-platform/news-collector/model"
 	"os"
 )
 import "fmt"
 import "encoding/json"
 
-//configuration structure
-type Config struct {
-	Cassandra     Cassandra     `json:"Cassandra"`
-	KafkaBrokers  string        `json:"KafkaBrokers"`
-	Version       Version       `json:"Version"`
-	GoogleNewsAPI GoogleNewsAPI `json:"GoogleNewsAPI"`
-}
-
-//google news api configuration
-type GoogleNewsAPI struct {
-	Url    string `json:"Url" envconfig:"Url"`
-	APIKey string `json:"APIKey" envconfig:"APIKey"`
-}
-
-//cassandra configuration
-type Cassandra struct {
-	Host     string `json:"Host" envconfig:CASSANDRA_HOST"`
-	Keyspace string `json:"Keyspace" envconfig:"CASSANDRA_KEYSPACE"`
-}
-
-//microservice version structure
-type Version struct {
-	SolutionName    string `json:"SolutionName"    envconfig:"NC_SOLUTION_NAME"`
-	ServiceName     string `json:"ServiceName"     envconfig:"NC_SERVICE_NAME"`
-	ServiceProvider string `json:"ServiceProvider" envconfig:"NC_SERVICE_PROVIDER"`
-	Number          string `json:"Number" envconfig:"NC_NUMBER"`
-}
-
 //Load configuration from the file
-func LoadConfiguration(file string) Config {
-	var config Config
+func LoadConfiguration(file string) model.Config {
+	var config model.Config
 
 	//open file and read configuration
 	configFile, err := os.Open(file)

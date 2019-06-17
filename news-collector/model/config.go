e495f6758fc132a96f3c9f808d75986f6ac6f9e1
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-17 22:29:10 +0530

    moved the models to separate package - model

---
+++ news-collector/model/config.go
@@ -0,0 +1,31 @@
+package model
+
+//configuration structure
+type Config struct {
+	Cassandra     Cassandra     `json:"Cassandra"`
+	KafkaBrokers  string        `json:"KafkaBrokers"`
+	Topic         string        `json:"Topic"`
+	Version       Version       `json:"Version"`
+	GoogleNewsAPI GoogleNewsAPI `json:"GoogleNewsAPI"`
+	ListernPort   string        `json:"ListernURL"`
+}
+
+//google news api configuration
+type GoogleNewsAPI struct {
+	Url    string `json:"Url" envconfig:"Url"`
+	APIKey string `json:"APIKey" envconfig:"APIKey"`
+}
+
+//cassandra configuration
+type Cassandra struct {
+	Host     string `json:"Host" envconfig:CASSANDRA_HOST"`
+	Keyspace string `json:"Keyspace" envconfig:"CASSANDRA_KEYSPACE"`
+}
+
+//microservice version structure
+type Version struct {
+	SolutionName    string `json:"SolutionName"    envconfig:"NC_SOLUTION_NAME"`
+	ServiceName     string `json:"ServiceName"     envconfig:"NC_SERVICE_NAME"`
+	ServiceProvider string `json:"ServiceProvider" envconfig:"NC_SERVICE_PROVIDER"`
+	Number          string `json:"Number" envconfig:"NC_NUMBER"`
+}

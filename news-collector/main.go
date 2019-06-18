faf52ac0df7ea9deef7344b671fe48e1395e8d10
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-18 22:22:02 +0530

    read configuration from the config file

--- news-collector/main.go
+++ news-collector/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/nik/news-platform/news-collector/services"
+import (
+	"github.com/nik/news-platform/news-collector/config"
+	"github.com/nik/news-platform/news-collector/services"
+)
 
 func main() {
-	services.ScrapeNewsHeadlines()
+	config := config.LoadConfiguration("news-collector/config/config.json")
+	services.Init(config.GoogleNewsAPI.Url, config.GoogleNewsAPI.APIKey)
+	services.ScrapeNewsHeadlines(config)
 }

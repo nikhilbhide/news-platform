490329caeb52c592aea3ec118be2349d67869c0c
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-18 22:13:44 +0530

    moved the models to separate package - model

--- news-store/services/HeadlinesConsumer.go
+++ news-store/services/HeadlinesConsumer.go
@@ -2,29 +2,12 @@ package services
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/nik/news-platform/common-platform/cassandra"
 	"github.com/nik/news-platform/common-platform/kafka"
+	"github.com/nik/news-platform/news-store/model"
+	"github.com/nik/news-platform/news-store/repository"
 	"time"
 )
-
-type Newsheadlines struct {
-	Status       string `json:"status"`
-	TotalResults int    `json:"totalResults"`
-	Articles     []struct {
-		Source struct {
-			ID   string `json:"id"`
-			Name string `json:"name"`
-		} `json:"source"`
-		Author      string    `json:"author"`
-		Title       string    `json:"title"`
-		Description string    `json:"description"`
-		URL         string    `json:"url"`
-		URLToImage  string    `json:"urlToImage"`
-		PublishedAt time.Time `json:"publishedAt"`
-		Content     string    `json:"content"`
-	} `json:"articles"`
-}
 
 func ListenAndProcessArticles() {
 	//create a consumer
@@ -34,29 +17,25 @@ func ListenAndProcessArticles() {
 	for {
 		msg, _ := consumer.ReadMessage(-1)
 
-		//process messages
 		if msg != nil {
-			//transform the message into articles
-			res := Newsheadlines{}
-			if err := json.Unmarshal(msg.Value, &res); err != nil {
-				panic(err)
-			}
-
-			//connect to cassandra cluster and insert the articles into news_headlines
-			session := cassandra.ConnectToCluster()
-			defer cassandra.CloseSession(session)
-			for _, article := range res.Articles {
-				if err := session.Query(`
-      					INSERT INTO news_headlines (country, author, content, date, description, url, published_at) VALUES (?, ?, ?,?, ?, ?, ?)`,
-					"us", article.Author, article.Content, article.PublishedAt.Format("01-02-2006"), article.Description, article.URL, article.PublishedAt).Exec(); err != nil {
-					fmt.Println(err)
-				} else {
-
+			//extract the key as its the country name
+			country := msg.Key
+			//process messages
+			if msg != nil {
+				//transform the message into articles
+				res := model.Newsheadlines{}
+				if err := json.Unmarshal(msg.Value, &res); err != nil {
+					panic(err)
 				}
-			}
-		}
 
-		//sleep for 1000
-		time.Sleep(time.Microsecond)
+				//connect to cassandra cluster and insert the articles into news_headlines
+				session := cassandra.ConnectToCluster()
+				defer cassandra.CloseSession(session)
+				repository.Insert(session, string(country), res)
+			}
+
+			//sleep for 1000
+			time.Sleep(time.Microsecond)
+		}
 	}
 }

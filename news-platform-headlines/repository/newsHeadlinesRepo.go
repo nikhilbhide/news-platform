7ea819e117ddc258988c309a3217413745cc9e1e
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-20 21:03:28 +0530

    added api to select data based on country

--- news-platform-headlines/repository/newsHeadlinesRepo.go
+++ news-platform-headlines/repository/newsHeadlinesRepo.go
@@ -1,12 +1,14 @@
 package repository
 
 import (
+	"fmt"
 	"github.com/gocql/gocql"
-	"github.com/nik/news-platform/news-store/model"
+	"github.com/nik/news-platform/news-platform-headlines/model"
 )
 
 const headlinesTable = "news_headlines"
-const insertQuery = "INSERT INTO news_headlines (country, author, content, date, description, url, published_at,title, source_id, source_name) VALUES (?, ?, ?,?, ?, ?, ?,?,?)"
+const insertQuery = "INSERT INTO news_headlines (country, author, content, date, description, url, published_at,title, source_id, source_name) VALUES (?, ?, ?,?, ?, ?, ?,?,?,?)"
+const selectQuery = "select content from news_headlines where country = ?"
 
 //method to insert the records into the table
 func Insert(session *gocql.Session, country string, input model.Newsheadlines) {
@@ -15,5 +17,13 @@ func Insert(session *gocql.Session, country string, input model.Newsheadlines) {
 			country, article.Author, article.Content, article.PublishedAt.Format("01-02-2006"), article.Description, article.URL, article.PublishedAt, article.Title, article.Source.ID, article.Source.Name).Exec(); err != nil {
 			panic(err)
 		}
+	}
+}
+
+func GetHeadlinesByCountry(session *gocql.Session, country string) {
+	var text string
+	iter := session.Query(selectQuery, country).Iter()
+	for iter.Scan(&text) {
+		fmt.Println("content:", text)
 	}
 }

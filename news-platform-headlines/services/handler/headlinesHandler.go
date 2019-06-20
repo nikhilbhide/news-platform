c2dc133405d2f881c20f1ea7cecf7fd06873a718
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-20 22:01:58 +0530

    added a handler to retrieve the headlines

---
+++ news-platform-headlines/services/handler/headlinesHandler.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/nik/news-platform/common-platform/cassandra"
+	"github.com/nik/news-platform/news-platform-headlines/repository"
+	"net/http"
+	"time"
+)
+
+func GetTodaysHeadlines(w http.ResponseWriter, r *http.Request) {
+	//extract query parameters
+	queryMap := r.URL.Query()
+
+	//check whether query parameter query is present
+	country := queryMap.Get("country")
+	fmt.Println(country)
+
+	//connect to cassandra cluster and insert the articles into news_headlines
+	session := cassandra.ConnectToCluster()
+	defer cassandra.CloseSession(session)
+	date := time.Now()
+	//Format MM-DD-YYYY
+	fmt.Println(date.Format("01-02-2006"))
+	repository.GetHeadlinesByCountry(session, country)
+}

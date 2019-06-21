48312ad4a1417caa77734d5c77df7916d61f6c88
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-21 22:19:39 +0530

    added code for selecting data from article_db table based on country

--- news-platform-headlines/repository/newsHeadlinesRepo.go
+++ news-platform-headlines/repository/newsHeadlinesRepo.go
@@ -1,14 +1,14 @@
 package repository
 
 import (
-	"fmt"
 	"github.com/gocql/gocql"
 	"github.com/nik/news-platform/news-platform-headlines/model"
+	"time"
 )
 
 const headlinesTable = "news_headlines"
 const insertQuery = "INSERT INTO news_headlines (country, author, content, date, description, url, published_at,title, source_id, source_name) VALUES (?, ?, ?,?, ?, ?, ?,?,?,?)"
-const selectQuery = "select content from news_headlines where country = ?"
+const selectQuery = "SELECT country,content,author,published_at from news_headlines where country = ?"
 
 //method to insert the records into the table
 func Insert(session *gocql.Session, country string, input model.Newsheadlines) {
@@ -20,10 +20,31 @@ func Insert(session *gocql.Session, country string, input model.Newsheadlines) {
 	}
 }
 
-func GetHeadlinesByCountry(session *gocql.Session, country string) {
-	var text string
+//gets the headlines from the database
+func GetHeadlinesByCountry(session *gocql.Session, country string) []model.NewsheadlinesResponse {
+	var (
+		content      string
+		published_at time.Time
+		author       string
+	)
+
+	var headlines []model.NewsheadlinesResponse
+
+	//gets the data from table
 	iter := session.Query(selectQuery, country).Iter()
-	for iter.Scan(&text) {
-		fmt.Println("content:", text)
+
+	//iterate over the results and populate headlines response
+	for iter.Scan(&country, &content, &author, &published_at) {
+		newsHeadlinesInstance := model.NewsheadlinesResponse{
+			County:       country,
+			Content:      content,
+			Published_At: published_at,
+			Author:       author,
+		}
+
+		//append the headlines
+		headlines = append(headlines, newsHeadlinesInstance)
 	}
+
+	return headlines
 }

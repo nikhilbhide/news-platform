575918fbfb8c3d20d1652a0aa18ab78084a59069
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-29 22:39:54 +0530

    added implementation for scraping and storing metadata into cassandra

--- news-web-scraper/googleNewsApiCountryScraper.go
+++ news-web-scraper/googleNewsApiCountryScraper.go
@@ -1,66 +1,16 @@
 package main
 
-import (
-	"github.com/PuerkitoBio/goquery"
-	"io"
-	"log"
-	"net/http"
-	"os"
-)
+import "github.com/nik/news-platform/news-web-scraper/handlers/scraper"
 
 //a set of countries scraped from web page
-var countrySet map[string]bool
+var countryMetadata map[string]string
 var countires []string
 
 //return a slice of countries
-func GetCountries() []string {
+func extractCountries() []string {
 	return countires
 }
 
 func main() {
-	countrySet = make(map[string]bool) // New empty set
-
-	// Make HTTP request
-	response, err := http.Get("https://newsapi.org/sources")
-	//response, err := client.Do(request)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-
-	// Create a goquery document from the HTTP response
-	document, err := goquery.NewDocumentFromReader(response.Body)
-	if err != nil {
-		log.Fatal("Error loading HTTP response body. ", err)
-	}
-
-	// Make request
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-
-	// Copy data from the response to standard output
-	_, err = io.Copy(os.Stdout, response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Find all links and process them with the function
-	getCountries := func(index int, element *goquery.Selection) {
-		// See if the href attribute exists on the element
-		country, exists := element.Attr("data-country")
-		if exists {
-			countrySet[country] = true
-		}
-	}
-
-	//extract the countries from web page
-	document.Find("div").Each(getCountries)
-
-	//iterate over map and retrieve countires
-	for key, _ := range countrySet {
-		countires = append(countires, key)
-	}
+	scraper.ScrapeAndStoreMetaData()
 }

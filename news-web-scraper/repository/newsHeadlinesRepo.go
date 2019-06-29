129f924a9017ea7b83100ae4468fa167ecfa72f1
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-29 21:17:12 +0530

    added repo for inserting metadata

---
+++ news-web-scraper/repository/newsHeadlinesRepo.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"github.com/gocql/gocql"
+	"github.com/nik/news-platform/common-platform/cassandra"
+	"github.com/nik/news-platform/news-web-scraper/model"
+	"log"
+)
+
+const headlinesTable = "news_headlines"
+const insertQuery = "INSERT INTO news_headlines_web_country_metadata (country, shortname) VALUES (?, ?,) IF NOT EXISTS"
+
+//method to insert the records into the table
+func InsertGoogleNewsHeadlinesMetadata(response []model.GoogleNewslinesMetadata) {
+	//create a session object
+	session := cassandra.ConnectToCluster()
+	defer cassandra.CloseSession(session)
+
+	//create a batch
+	batch := gocql.NewBatch(gocql.LoggedBatch)
+	for counter := 0; counter < len(response); counter++ {
+		batch.Query(insertQuery, counter, response[counter].Country, response[counter].Shortname)
+	}
+
+	//execute a batch of cassandra
+	err := session.ExecuteBatch(batch)
+	if err != nil {
+		log.Fatal("Insert failed", err)
+	}
+}

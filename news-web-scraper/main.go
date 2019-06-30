30604b3fe4f08d3416ba0b023d89d20821afc16f
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-30 16:50:43 +0530

    added configuration support and logger

--- news-web-scraper/main.go
+++ news-web-scraper/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/nik/news-platform/news-web-scraper/handlers/scraper"
+import (
+	"github.com/nik/news-platform/common-platform/logger"
+	"github.com/nik/news-platform/news-web-scraper/config"
+	"github.com/nik/news-platform/news-web-scraper/handlers/scraper"
+)
 
 func main() {
+	//load the configuration
+	config := config.LoadConfiguration("news-web-scraper/config/config.json")
+	//set the logger to use advanced logging
+	logger := logs.InitLogger(config.Logger.LogPath)
+	logger.Info("Bootstrapping the application")
 	//start scraping google website and store metadata
-	scraper.ScrapeAndStoreMetaData()
+	scraper.ScrapeAndStoreMetaData(config)
 }

4c3939eb7dbc1db6fd847ffeed5b3c24b59f35a5
nikhilbhide <nikhilbhide15@gmail.com>
2019-06-30 14:52:13 +0530

    added configuration support for the microservice

---
+++ news-web-scraper/model/config.go
@@ -0,0 +1,22 @@
+package model
+
+//configuration structure
+type Config struct {
+	Cassandra struct {
+		Host     string `json:"Host"`
+		Keyspace string `json:Keyspace'`
+	} `json:"Cassandra"`
+	KafkaBrokers   string         `json:"KafkaBrokers"`
+	Version        Version        `json:"Version"`
+	WebsiteScraper WebsiteScraper `json:"WebsiteScraper"`
+}
+
+type Version struct {
+	SolutionName    string `json:"SolutionName"    envconfig:"TKS_SOLUTION_NAME"`
+	ServiceName     string `json:"ServiceName"     envconfig:"TKS_SERVICE_NAME"`
+	ServiceProvider string `json:"ServiceProvider" envconfig:"TKS_SERVICE_PROVIDER"`
+}
+
+type WebsiteScraper struct {
+	GoogleNewsMetadataAPI string `json:"GoogleNewsMetadataAPI"    envconfig:"GOOGLE_NEWS_METADATA_API"`
+}

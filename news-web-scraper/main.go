0a8bdcffcfdfb7d494faf097377df9c7b8c130ac
nikhilbhide <nikhilbhide15@gmail.com>
2019-07-04 22:47:05 +0530

    created http server

--- news-web-scraper/main.go
+++ news-web-scraper/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"github.com/nik/news-platform/common-platform/logger"
 	"github.com/nik/news-platform/news-web-scraper/config"
+	"github.com/nik/news-platform/news-web-scraper/handlers/metadata/google"
 	"github.com/nik/news-platform/news-web-scraper/handlers/scraper"
+	"net/http"
 )
 
 func main() {
@@ -14,4 +16,16 @@ func main() {
 	logger.Info("Bootstrapping the application")
 	//start scraping google website and store metadata
 	scraper.ScrapeAndStoreMetaData(config)
+
+	//setting up web server middlewares
+	/*middlewareManager := alice.New(
+	transactionId.AddIDRequestMiddleware,transactionId.AddIDRequestMiddleware).
+	Then(router.CreateRouter())
+	*/
+	logger.Info("http handlers are initialized")
+
+	//start listening
+	http.HandleFunc("/view/", handler.GetCountryMetadata)
+
+	http.ListenAndServe("localhost:8083", nil)
 }
